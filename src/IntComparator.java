7f420aecd5bf11746d52631c3c9bd7392baffa8d
Sh1nePr0 <108802777+Sh1nePr0@users.noreply.github.com>
2023-09-05 22:12:56 +0300

    Create IntComparator.java

---
+++ src/IntComparator.java
@@ -0,0 +1,7 @@
+import java.util.Comparator;
+public class IntComparator implements Comparator<Integer>{
+    @Override
+    public int compare(Integer a , Integer b){
+        return a.compareTo(b);
+    }
+}
